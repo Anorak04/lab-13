10b7850ea645499e067f518c9501aedff2c411e2
Enkh-Amgalan Altanshagai <118392264+Anorak04@users.noreply.github.com>
2022-11-16 20:51:53 +0000

    Added a function to display name and netID and something about git

---
+++ .ccls-cache/@home@runner@TastyJauntyStaff/211-f22-collab-github@collab.c
@@ -0,0 +1,38 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void ealtan3_vkaspr2(){
+  printf("Vira Kasprova vkaspr2\n");
+  printf("Enkh-Amgalan Altanshagai ealtan3\n");
+  printf("Something we learned about GIT: On replit we can clone a repository either form the command line or from Version Control");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    // reed_ekidan2();
+  ealtan3_vkaspr2();
+    
+    return 0;
+}
